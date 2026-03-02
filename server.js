d0ee3be80d303d9c4008dfd9988fff6068ea4862
Андрій <andreygerc11@gmail.com>
2026-03-02 10:41:40 +0200

    server.js

--- server.js
+++ server.js
@@ -2,21 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-
 app.use(cors());
 
-// Твоє останнє посилання (v7)
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwXS6FZEC7IXqcRRYtMxLpQvbyhPyiaBit-kgpI5vshqLA1Z8lXtzypObV-09lwdjq-/exec";
+// Твоє нове посилання (v8)
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx-ZZyOgiOwl67Pvypqw2Tej6ylWGFATEPAwqE6zpuVDMIzg9XPRBfyhPkoo9R1NB_C/exec";
 
 app.get('/api/music', async (req, res) => {
     try {
-        const response = await axios.get(GOOGLE_SCRIPT_URL, { 
-            maxRedirects: 5 
-        });
+        const response = await axios.get(GOOGLE_SCRIPT_URL, { maxRedirects: 5 });
         res.json(response.data);
     } catch (error) {
-        console.error("Помилка зв'язку:", error.message);
-        res.status(500).json({ error: "Помилка зв'язку з Google Scripts" });
+        res.status(500).json({ error: "Помилка зв'язку з Google" });
     }
 });
 
